12d2c1cdcd7383e31a33fa0091b44dec10d02592
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-09-06 09:27:29 +0700

    add connection

---
+++ src/main/java/com/example/module3_exam/config/ConnectionDB.java
@@ -0,0 +1,34 @@
+package com.example.module3_exam.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    public static final String URL = "jdbc:mysql://localhost:3306/Case_Study";
+    public static final String USER = "root";
+
+    public static final String PASSWORD = "chetdicon";
+
+    public ConnectionDB() {
+    }
+
+
+    public static Connection getConnect() {
+        Connection connect = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Ket noi thanh cong");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connect;
+
+    }
+
+    public static void main(String[] args) {
+        getConnect();
+    }
+}
